90f09e104f43eae54b36bc266f63c62fa657c5b2
Nicollas Diniz <101371415+Dinizim@users.noreply.github.com>
2023-06-06 13:04:57 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
+
 const form = document.getElementById('form')
 const SignUp = document.querySelectorAll('.cadastro')
 const ErrorMSG = document.querySelectorAll('.error-msg')
 const input = document.querySelectorAll('.input')
-//pegar email regex
-const EmailRegex =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 
 //function que criar e remove o erro
-function setError(index){
+setError = (index) => {
     SignUp[index].style.border = '1px solid #ff7a7a'
     ErrorMSG[index].style.display = 'block'
 }
